f452e1093839478bf28884dfd1d869118629d8b7
cleverlight <github.account@cleveraspaint.com>
2014-04-10 20:32:39 +0100

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,22 @@
+class doopencv (
+
+  # class arguments
+  # ---------------
+  # setup defaults
+
+  $user = 'root'
+
+  # end of class arguments
+  # ----------------------
+  # begin class
+
+) {
+
+  case $operatingsystem {
+    centos, redhat, fedora: {
+    }
+    ubuntu, debian: {
+    }
+  }
+    
+}
